d35840d840137ead9c065e8ed7302be436f523a1
Jolene Le <nga.le@georgebrown.ca>
2020-07-30 00:43:26 -0400

    Update index.js
    Add new routes

--- index.js
+++ index.js
@@ -14,12 +14,24 @@ app.get('/', (req, res) => {
   res.json({ info: 'Node.js, Express, and Postgres API' });
 });
 
-// Routes
+// Routes tasks
 app.get('/tasks', db.getTasks);
 app.get('/tasks/:id', db.getTaskById);
 app.post('/tasks', db.createTask);
 app.put('/tasks/:id', db.updateTask);
 app.put('/tasks/:id', db.deleteTask);
+// Routes users
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.put('/users/:id', db.deleteUser);
+//Routes todolist
+app.get('/todolist', db.getTodo);
+app.get('/todolist/:id', db.getTodoById);
+app.post('/todolist', db.createTodo);
+app.put('/todolist/:id', db.updateTodo);
+app.put('/todolist/:id', db.deleteTodo);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
